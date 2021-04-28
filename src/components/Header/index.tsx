dd7387791e1137f52f96f9498d9b3ab42fc46350
Bruno Barbaro <bruno.barbaro1995@gmail.com>
2021-04-28 03:39:24 -0300

    Inclusão botão Dark/Light Theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,17 +4,25 @@ import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
 import styles from './styles.module.scss';
 
-export function Header() {
+export function Header({theme, toggleTheme}) {
   const currentDate = format(new Date(), 'EEEEEE, d MMMM', { locale:ptBR });
 
   return (
     <header className={styles.headerContainer}>
       <Link href="/">
-        <img src="/logo-dark.svg" alt="Podcastr" />
+        <img 
+          src={theme == "theme-light" ?  "/logo.svg" : "/logo-dark.svg"}
+          alt="Podcastr" />
       </Link>
       
       <p>O melhor para você ouvir, sempre</p>
       <span>{currentDate}</span>
+      <button className={styles.toggleTheme} onClick={() => toggleTheme()}>
+        <img 
+          src={theme == "theme-light" ?  "/dark-mode.svg" : "/light-mode.svg"} 
+          alt={theme == "theme-light" ?  "dark-mode" : "light-mode"} 
+          />
+      </button>
     </header>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,23 @@ import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 import {  PlayerContextProvider } from '../contexts/PlayerContext';
 import '../styles/global.scss';
+
 import styles from '../styles/app.module.scss';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+
+  const [theme, setTheme] = useState("theme-dark");
+
+  const toggleTheme = () => {
+      theme == 'theme-light' ? setTheme('theme-dark') : setTheme('theme-light')
+  };
+  
   return (
     <PlayerContextProvider>
-      <div className={styles.wrapper}>
+      <div className={`${styles.wrapper} ${theme}`}>
         <main>
-        <Header/>
+          <Header theme={theme} toggleTheme={toggleTheme} />
           <Component {...pageProps} />
         </main>
         <Player/>
